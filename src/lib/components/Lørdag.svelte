4f66316938f1ef77f43b15682aefc775046caa38
Ludvig <ludvig.vigerust@evidi.com>
2023-09-23 18:16:03 +0200

    push

--- src/lib/components/Lørdag.svelte
+++ src/lib/components/Lørdag.svelte
@@ -1,64 +1,44 @@
 <script lang="ts">
 	import { ExternalLink } from '$icons'
-	import { onMount } from 'svelte'
-	import { animate, inView } from 'motion'
-
-	let opacity = 'opacity-100'
-
-	onMount(() => {
-		if (window.innerWidth < 640) {
-			opacity = 'opacity-0'
-			inView('#animationCard', () => {
-				animate(
-					'#animationCard',
-					{ y: [100, 0], opacity: 1 },
-					{ delay: 0.2, duration: 0.5, easing: [0.17, 0.55, 0.55, 1] }
-				)
-			})
-		}
-	})
 </script>
 
-<div id="animationCard" class={opacity}>
-	<div
-		class="w-full shadow bg-base-200 p-4 rounded-md text-base-content border-none ring-1 ring-neutral-700/10"
-	>
-		<h2 class="font-medium text-gray-500 mb-6 text-sm font-Display">Lørdag</h2>
+<div
+	class="w-full shadow bg-base-200 p-4 rounded-md text-base-content border-none ring-1 ring-neutral-700/10"
+>
+	<h2 class="font-medium text-gray-500 mb-6 text-sm font-Display">Lørdag</h2>
 
-		<div class="content my-4">
-			<a href="https://maps.app.goo.gl/vsiktFV4mBWjPwN26" target="_blank">
-				<hgroup class="my-4">
-					<h3 class="text-gray-100 font-medium">Hovedkonferansen 📣</h3>
-
-					<div class="grid grid-cols-2">
-						<p class="text-sm leading-6">Sted:</p>
-						<p class="text-sm leading-6">Comwell Portside Copenhagen</p>
-						<p class="text-sm leading-6">Tid:</p>
-						<p class="text-sm leading-6">8:30 - 17:30</p>
-					</div>
-				</hgroup>
-			</a>
-		</div>
-
-		<a
-			href="https://nordicsummit.info/agenda/"
-			target="_blank"
-			class="btn mt-6 normal-case font-medium btn-primary w-full"
-			>Oppdatert agenda <ExternalLink /></a
-		>
-		<div class="divider" />
-
-		<a href="https://maps.app.goo.gl/xgT8dhNUGwwHQkzx6" target="_blank">
+	<div class="content my-4">
+		<a href="https://maps.app.goo.gl/vsiktFV4mBWjPwN26" target="_blank">
 			<hgroup class="my-4">
-				<h3 class="text-gray-100 font-medium">Middag 🍱</h3>
+				<h3 class="text-gray-100 font-medium">Hovedkonferansen 📣</h3>
 
 				<div class="grid grid-cols-2">
 					<p class="text-sm leading-6">Sted:</p>
-					<p class="text-sm leading-6">Food Club Nørrebro</p>
+					<p class="text-sm leading-6">Comwell Portside Copenhagen</p>
 					<p class="text-sm leading-6">Tid:</p>
-					<p class="text-sm leading-6">19:30 -</p>
+					<p class="text-sm leading-6">8:30 - 17:30</p>
 				</div>
 			</hgroup>
 		</a>
 	</div>
+
+	<a
+		href="https://nordicsummit.info/agenda/"
+		target="_blank"
+		class="btn mt-6 normal-case font-medium btn-primary w-full">Oppdatert agenda <ExternalLink /></a
+	>
+	<div class="divider" />
+
+	<a href="https://maps.app.goo.gl/xgT8dhNUGwwHQkzx6" target="_blank">
+		<hgroup class="my-4">
+			<h3 class="text-gray-100 font-medium">Middag 🍱</h3>
+
+			<div class="grid grid-cols-2">
+				<p class="text-sm leading-6">Sted:</p>
+				<p class="text-sm leading-6">Food Club Nørrebro</p>
+				<p class="text-sm leading-6">Tid:</p>
+				<p class="text-sm leading-6">19:30 -</p>
+			</div>
+		</hgroup>
+	</a>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script>
 	import { Fredag, Lørdag, Hotell, Informasjon, addToast } from '$components'
 	import { onMount } from 'svelte'
-	import { animate, inView } from 'motion'
 
 	function create() {
 		addToast({
@@ -13,14 +12,6 @@
 
 	onMount(() => {
 		create()
-
-		inView('#animationSelector', () => {
-			animate(
-				'.animationCard2',
-				{ y: [100, 0], opacity: 1 },
-				{ delay: 0.2, duration: 0.5, easing: [0.17, 0.55, 0.55, 1] }
-			)
-		})
 	})
 </script>
 
@@ -36,17 +27,12 @@
 </div>
 
 <div class="pb-2">
-	<h2
-		id="animationSelector"
-		class="font-display ml-1 mb-3 text-lg font-medium tracking-tight text-teal-600 sm:text-4xl animationCard2 opacity-0"
-	>
+	<h2 class="font-display ml-1 mb-3 text-lg font-medium tracking-tight text-teal-600 sm:text-4xl">
 		Annen informasjon
 	</h2>
 
 	<ul class="flex flex-col gap-y-6 lg:space-y-0 lg:grid lg:grid-cols-2 lg:gap-6">
-		<div class="animationCard2 opacity-0">
-			<Hotell />
-		</div>
+		<Hotell />
 		<Informasjon />
 	</ul>
 </div>
